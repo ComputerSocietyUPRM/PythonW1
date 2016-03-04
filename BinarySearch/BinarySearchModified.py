f51e75a9dc19355290e10bb95d0902e285978bac
Juan G. Cruz Hernandez <juan.cruz28@upr.edu>
2016-03-04 03:52:44 -0400

    Working version of "BinarySearch"
    New file added fixing errors in previous file.
    Note that new file does not have 'try' and 'except' statements and these still need to be added.

---
+++ BinarySearch/BinarySearchModified.py
@@ -0,0 +1,15 @@
+high = int(input("From 1 to wich number do you want the game to be? "))
+low = 1
+num = (high + low) // 2
+tries = 0
+while low != num or high != num:
+	tries += 1
+	answer = str(input("Is you number higher than '" + str(num) + "' [y/n] "))
+	if answer == "y":
+		low = num + 1
+	elif answer == "n":
+		high = num
+	else:
+		print("That is not a valid answer.")
+	num = (high + low) // 2
+print("Number of guesses '" + str(tries) + "'. Your number is '" + str(num)) + "'."
