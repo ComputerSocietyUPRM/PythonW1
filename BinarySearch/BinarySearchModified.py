3cb60b24434560814e22166500121ebe7dd55528
Juan G. Cruz Hernandez <juan.cruz28@upr.edu>
2016-03-13 23:53:06 -0400

    Syntax error fixed
    Fixed syntax errors in the last print and added 'maximun number of guesses needed message'

--- BinarySearch/BinarySearchModified.py
+++ BinarySearch/BinarySearchModified.py
@@ -1,4 +1,6 @@
+from math import log, ceil
 high = int(input("From 1 to wich number do you want the game to be? "))
+print("Maximun number of guesses needed is '" + str(ceil(log(high, 2))) + "'")
 low = 1
 num = (high + low) // 2
 tries = 0
@@ -12,4 +14,4 @@ while low != num or high != num:
 	else:
 		print("That is not a valid answer.")
 	num = (high + low) // 2
-print("Number of guesses '" + str(tries) + "'. Your number is '" + str(num)) + "'."
+print("Number of guesses '" + str(tries) + "'. Your number is '" + str(num) + "'.")
